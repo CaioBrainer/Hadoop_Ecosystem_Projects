06a4c700cb1161d4d7947104941bfd9d0bb67639
Caio Brainer <92734524+CaioBrainer@users.noreply.github.com>
2024-02-02 16:53:20 -0300

    Delete mapreduce_big_data directory

--- mapreduce_big_data/sci_mapper.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*-coding:utf-8 -*
-import sys
-
-for line in sys.stdin:
-	dados = line.split('\t')
-	if len(dados) == 6:
-		data,DOI,identificador,paises,Localidade,coordenadas = dados
-		print('%s\t%s'%(paises, 1))
